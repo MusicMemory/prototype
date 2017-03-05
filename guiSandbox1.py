8f9a7ce06b32d53e1e6f3cff6d3dacbd6eda2eaf
Jörg Vollmer <javacook@gmx.de>
2017-03-06 00:45:58 +0100

    initial

---
+++ guiSandbox1.py
@@ -0,0 +1,27 @@
+from Tkinter import *
+
+window = Tk()
+window.title("Mein erstes GUI-Programm")
+window.geometry("300x300")
+
+myImage = PhotoImage(file="Sun.png")
+
+def ausgabe():
+    print "Mache was"
+
+graphicalButton = Button(window, text="Klick mich!", command=ausgabe, image=myImage)
+graphicalButton.pack()
+
+inputField = Entry(window)
+inputField.pack()
+
+aLabel = Label(text="Initialer Text")
+aLabel.pack()
+
+def lesen():
+    aLabel.configure(text=inputField.get())
+
+standardButton = Button(window, text="Klick mich!", command=lesen)
+standardButton.pack()
+
+mainloop()
